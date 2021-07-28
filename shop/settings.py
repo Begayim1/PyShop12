c9efcabffb2fb37dff4ccc5f388c7d7bc6a7b76a
begaim <kyrgyzalieva12@gmail.com>
2021-07-28 16:18:04 +0600

    Add registration and login.

--- shop/settings.py
+++ shop/settings.py
@@ -39,6 +39,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+
+    # libs
+    'crispy_forms',
+
     # my apps
     'product',
     'account',
@@ -136,3 +140,6 @@ STATICFILES_DIRS = (
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+LOGOUT_REDIRECT_URL = '/'
+LOGIN_REDIRECT_URL = '/'
