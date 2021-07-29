6b6543c28252db0489ee762e06e720aca9c737e3
begaim <kyrgyzalieva12@gmail.com>
2021-07-29 15:56:58 +0600

    Add permissions, filtration, seaech, pagination.

--- account/urls.py
+++ account/urls.py
@@ -2,9 +2,14 @@ from django.contrib.auth.views import LogoutView
 from django.urls import path
 
 from account.views import *
+from product.class_views import SearchListView, ProductUpdateView, ProductDeleteView
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
     path('login/', SingInView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
+    path('product/update/<int:id>', ProductUpdateView.as_view(), name='update_product'),
+    path('product/delete/<int:id>', ProductDeleteView.as_view(), name='update_product'),
+    path('search', SearchListView.as_view(), name='search'),
+
 ]
--- product/class_views.py
+++ product/class_views.py
@@ -1,8 +1,27 @@
+from django.contrib.auth.mixins import UserPassesTestMixin
+from django.db.models import Q
 from django.urls import reverse
 from django.views.generic import ListView, DetailView, CreateView, UpdateView , DeleteView
 
 from product.forms import CreateProductForm, UpdateProductForm
 from product.models import Category, Product
+
+class SearchListView(ListView):
+    model = Product  # Product.objects.all()
+    template_name = 'products/search.html'
+    context_object_name = 'products'
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        # print(self.request.GET)
+        q = self.request.GET.get('q')
+        if not q:
+            return Product.objects.none()
+        queryset = queryset.filter(Q(name__icontains=q) | Q(description__icontains=q))
+        return  queryset
+
+
+
 
 
 class CategoryListView(ListView):
@@ -15,6 +34,7 @@ class ProductListView(ListView):
     model = Product # Product.object.all()
     template_name = 'products/list.html'
     context_object_name = 'products'
+    paginate_by = 1
 
     def get_queryset(self):
         queryset = super().get_queryset()
@@ -23,6 +43,11 @@ class ProductListView(ListView):
         return queryset
 
 
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['category'] = self.kwargs.get('slug')
+        return context
+
 class ProductDetailView(DetailView):
     model = Product
     template_name = 'products/detail.html'
@@ -30,7 +55,14 @@ class ProductDetailView(DetailView):
     pk_url_kwarg = 'id'
 
 
-class ProductCreateView(CreateView):
+class IsAdminCheckMixin(UserPassesTestMixin):
+    def test_func(self):
+        return self.request.user.is_authenticated and self.request.user.is_superuser
+
+
+
+
+class ProductCreateView(IsAdminCheckMixin, CreateView):
     model = Product
     template_name = 'products/create_product.html'
     form_class = CreateProductForm
@@ -44,18 +76,21 @@ class ProductCreateView(CreateView):
 
 
 
-class ProductUpdateView(UpdateView):
+class ProductUpdateView(IsAdminCheckMixin, UpdateView):
     model = Product
     template_name = 'products/update_product.html'
     form_class = UpdateProductForm
     pk_url_kwarg = 'id'
+
+
+
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['product_form'] = self.get_form(self.get_form_class())
         return context
 
-class ProductDeleteView(DeleteView):
+class ProductDeleteView(IsAdminCheckMixin, DeleteView):
     model = Product
     template_name = 'products/delete_product.html'
     pk_url_kwarg = 'id'
